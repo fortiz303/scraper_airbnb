e3c16c6de8173e820e5ea0a786c8f205c8dfb98f
scraper24124 <106816768+scraper24124@users.noreply.github.com>
2022-06-04 14:37:59 +0100

    Add files via upload

---
+++ airbnb_scraper/items.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+from scrapy.loader.processors import MapCompose, TakeFirst, Join
+
+
+def remove_unicode(value):
+    return value.replace(u"\u201c", '').replace(u"\u201d", '').replace(u"\2764", '').replace(u"\ufe0f")
+
+class AirbnbScraperItem(scrapy.Item):
+
+    # Host Fields
+    is_superhost = scrapy.Field()
+    host_id = scrapy.Field()
+
+    # Room Fields
+    check_in = scrapy.Field()
+    check_out = scrapy.Field()
+    price = scrapy.Field()
+    url = scrapy.Field()
+    is_business_travel_ready = scrapy.Field()
+    is_fully_refundable = scrapy.Field()
+    is_new_listing = scrapy.Field()
+    lat = scrapy.Field()
+    lng = scrapy.Field()
+    localized_city = scrapy.Field()
+    localized_neighborhood = scrapy.Field()
+    listing_name = scrapy.Field(input_processor=MapCompose(remove_unicode))
+    person_capacity = scrapy.Field()
+    picture_count = scrapy.Field()
+    reviews_count = scrapy.Field()
+    room_type_category = scrapy.Field()
+    star_rating = scrapy.Field() # Rounded to .5 or .0 Avg Rating
+    avg_rating = scrapy.Field()
+    can_instant_book = scrapy.Field()
+    monthly_price_factor = scrapy.Field()
+    currency = scrapy.Field()
+    amt_w_service = scrapy.Field()
+    rate_type = scrapy.Field()
+    weekly_price_factor = scrapy.Field()
+    bathrooms = scrapy.Field()
+    bedrooms = scrapy.Field()
+    num_beds = scrapy.Field()
+    accuracy = scrapy.Field()
+    communication = scrapy.Field()
+    cleanliness = scrapy.Field()
+    location = scrapy.Field()
+    checkin = scrapy.Field()
+    value = scrapy.Field()
+    guest_satisfication = scrapy.Field()
+    host_reviews = scrapy.Field()
+    response_rate = scrapy.Field()
+    response_time = scrapy.Field()
+
+
+
